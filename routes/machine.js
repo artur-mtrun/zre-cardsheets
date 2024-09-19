5b407e60b63a0297c2bac65c3a9b60217f061148
artur-mtrun <artur@mtrun.pl>
2024-09-19 09:24:27 +0000

    add route to edit machine

--- routes/machine.js
+++ routes/machine.js
@@ -6,4 +6,6 @@ router.get('/machine-list', machineController.getMachines);
 router.get('/add-machine', machineController.getAddMachine);
 router.post('/add-machine', machineController.postAddMachine);
 router.post('/remove-machine', machineController.postRemoveMachine);
+router.get('/edit-machine/:id', machinesController.getEditMachine);
+router.post('/edit-machine', machinesController.postEditMachine);
 module.exports = router;
