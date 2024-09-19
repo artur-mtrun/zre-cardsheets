b4688bfb2354ae55eacfce15fcc8559f1f3baf8c
artur-mtrun <artur@mtrun.pl>
2024-09-19 09:28:58 +0000

    error:machinesController -> machineController

--- routes/machine.js
+++ routes/machine.js
@@ -6,6 +6,6 @@ router.get('/machine-list', machineController.getMachines);
 router.get('/add-machine', machineController.getAddMachine);
 router.post('/add-machine', machineController.postAddMachine);
 router.post('/remove-machine', machineController.postRemoveMachine);
-router.get('/edit-machine/:id', machinesController.getEditMachine);
-router.post('/edit-machine', machinesController.postEditMachine);
+router.get('/edit-machine/:id', machineController.getEditMachine);
+router.post('/edit-machine', machineController.postEditMachine);
 module.exports = router;
