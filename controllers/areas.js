f5d674206a3062a7c7b027d44720de91b31115f7
artur-mtrun <artur@mtrun.pl>
2024-10-20 17:56:03 +0000

    Dodanie obszaru

--- controllers/areas.js
+++ controllers/areas.js
@@ -34,9 +34,18 @@ exports.postAddArea = (req, res, next) => {
     if (!req.session.isLoggedIn) {
         return res.redirect('/login');
     }
+    const descript = req.body.descript;
+    if (!descript || descript.trim() === '') {
+        return res.status(400).render('areas/add-area', {
+            pageTitle: 'Dodaj obszar',
+            path: '/add-area',
+            errorMessage: 'Opis obszaru nie może być pusty.',
+            isAuthenticated: req.session.isLoggedIn,
+            isAdmin: req.session.isAdmin
+        });
+    }
     const area = new Area({
-        area_id: req.body.area_id,
-        descriript: req.body.descriript
+        descript: descript
     });
 
     area.save()
@@ -49,3 +58,70 @@ exports.postAddArea = (req, res, next) => {
     });
 };
 
+exports.getEditArea = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
+        return res.redirect('/login');
+    }
+    const areaId = req.params.areaId;
+    Area.findByPk(areaId)
+        .then(area => {
+            if (!area) {
+                return res.redirect('/area-list');
+            }
+            res.render('areas/edit-area', {
+                pageTitle: 'Edytuj obszar',
+                path: '/edit-area',
+                area: area,
+                isAuthenticated: req.session.isLoggedIn,
+                isAdmin: req.session.isAdmin
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            next(err);
+        });
+};
+
+exports.postEditArea = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
+        return res.redirect('/login');
+    }
+    const areaId = req.body.areaId;
+    const updatedDescript = req.body.descript;
+    Area.findByPk(areaId)
+        .then(area => {
+            if (!area) {
+                return res.redirect('/area-list');
+            }
+            area.descript = updatedDescript;
+            return area.save();
+        })
+        .then(result => {
+            res.redirect('/area-list');
+        })
+        .catch(err => {
+            console.log(err);
+            next(err);
+        });
+};
+
+exports.postDeleteArea = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
+        return res.redirect('/login');
+    }
+    const areaId = req.body.areaId;
+    Area.findByPk(areaId)
+        .then(area => {
+            if (!area) {
+                return res.redirect('/area-list');
+            }
+            return area.destroy();
+        })
+        .then(result => {
+            res.redirect('/area-list');
+        })
+        .catch(err => {
+            console.log(err);
+            next(err);
+        });
+};
--- routes/area.js
+++ routes/area.js
@@ -6,4 +6,9 @@ router.get('/area-list', areaController.getAreas);
 router.get('/add-area', areaController.getAddArea);
 router.post('/add-area', areaController.postAddArea);
 
+// Dodaj te nowe trasy
+router.get('/edit-area/:areaId', areaController.getEditArea);
+router.post('/edit-area', areaController.postEditArea);
+router.post('/delete-area', areaController.postDeleteArea);
+
 module.exports = router;
